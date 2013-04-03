0d2f14d6bd3df728bf6bc48de8e982ce0c4c49ee
Gennx <workgenevieve@gmail.com>
2013-04-02 20:13:32 -0700

    added to gitignore, Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobapp::Application.config.secret_token = '03b9431d5b59f70fc1335161f13cf91094308a0aada973e139e762168df442a42cf1c0c9d1d814525bd0950c5d8a0887fff71a2d9c2eafe6e0fcabaf564dec49'
